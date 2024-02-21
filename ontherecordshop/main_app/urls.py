f229b1283be843a3fb77fd58fce3557da85e0301
mherrera491 <mattherrera1@gmail.com>
2024-02-21 00:47:44 -0800

    auth working correctly

--- ontherecordshop/main_app/urls.py
+++ ontherecordshop/main_app/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 from . import views
 
 urlpatterns = [
@@ -6,5 +7,6 @@ urlpatterns = [
     path("products/", views.products_index, name='all_products'),
     path("cart/", views.cart, name='cart'),
     path("checkout/", views.checkout, name='checkout'),
+    path('accounts/logout/', LogoutView.as_view(next_page='/'), name='logout'),
 ]
 
